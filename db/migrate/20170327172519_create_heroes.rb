43bc24e1b307726bf67f76bfc0e6f490068a28ec
ZainW <zainthegreatcb@gmail.com>
2017-03-27 13:26:42 -0400

    scaffold for heroes

---
+++ db/migrate/20170327172519_create_heroes.rb
@@ -0,0 +1,22 @@
+class CreateHeroes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :heroes do |t|
+      t.string :name
+      t.string :profile_url
+      t.integer :base_str
+      t.integer :base_agi
+      t.integer :base_int
+      t.float :add_str
+      t.float :add_agi
+      t.float :add_int
+      t.integer :move_speed
+      t.float :turn_rate
+      t.string :attack_type
+      t.jsonb :talents
+      t.integer :attack_range
+      t.string :hero_type
+
+      t.timestamps
+    end
+  end
+end
