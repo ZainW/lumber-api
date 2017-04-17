ae5834742dfcb9ab64a67b41bc26ac3cd4160305
ZainW <zainthegreatcb@gmail.com>
2017-04-17 14:40:27 -0400

    abilities searchable by searchable hero and hotkey capatilized

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -46,7 +46,11 @@ class AbilitiesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_hero_ability
-      @ability = @hero.abilities.find_by!(id: params[:id]) if @hero
+      @ability = @hero.abilities.where("more->>'hotkey' = ?", "#{params[:hotkey]}").first if @hero
+      if @ability == nil
+        @ability = Ability.find_by!(name: "nil")
+      end
+
     end
 
     # Only allow a trusted parameter "white list" through.
@@ -54,6 +58,11 @@ class AbilitiesController < ApplicationController
       params.permit(:name, :more, :hero_id)
     end
     def set_hero
-      @hero = Hero.find_by!(name: params[:hero_name])
+      search = Hero.search "#{params[:hero_name]}", fields: [:name]
+      if search.results == []
+        Hero.find_by!(name: "nil")
+      else
+        @hero = search.results.first
+      end
     end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,10 @@
 class Ability < ApplicationRecord
   belongs_to :hero
   validates_presence_of :name, :hero_id
+  searchkick
+  def search_data
+    {
+      name: name
+    }
+  end
 end
