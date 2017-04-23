ff58d8772b8781851ba4ccd5f17dbd3f81d0a5a6
ZainW <zainthegreatcb@gmail.com>
2017-04-22 22:51:43 -0400

    somehting

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,11 +1,14 @@
 class BuildsController < ApplicationController
   before_action :get_items
   def make
-    @build = {:hp => 0, :str => 0, :agi => 0, :int => 0, :mana => 0, :armor => 0, :damage => 0, :hpregen => 0, :evasion => 0, :attackspeed => 0, :manaregen => 0, :movespeed => 0, :itemcost => 0}
+    @build = {:hp => 0, :str => 0, :agi => 0, :int => 0, :mana => 0, :armor => 0, :damage => 0, :hpregen => 0, :evasion => 0, :attackspeed => 0, :manaregen => 0, :movespeed => 0, :itemcost => 0, :items => []}
     @items.each do |item|
       @build.keys.each do |key|
-        @build[key] += item.more[key.to_s].to_i
+        if key != :items
+          @build[key] += item.more[key.to_s].to_i
+        end
       end
+      @build[:items] << item.name
     end
     render json: @build
   end
