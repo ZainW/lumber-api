ec0575f402c2a0947346abb30e81fa395944088c
ZainW <zainthegreatcb@gmail.com>
2017-04-28 16:58:05 -0400

    programming that sucks less

--- db/seeds/1hero-auto-seeds.rb
+++ db/seeds/1hero-auto-seeds.rb
@@ -1,5 +1,10 @@
 require 'open-uri'
 require 'net/http'
+def trim_talents *talents
+  talents.each do |talent|
+    talent.gsub!(/'|\btalent\b/, "")
+  end
+end
 heroes = %w[ Alchemist Beastmaster Brewmaster Bristleback Centaur_Warrunner Clockwerk Dragon_Knight Earth_Spirit Earthshaker Elder_Titan Huskar Io Kunkka Legion_Commander Omniknight Phoenix Sven Timbersaw Tiny Treant_Protector Tusk Abaddon Axe Chaos_Knight Doom Lifestealer Lycan Magnus Night_Stalker Pudge Sand_King Slardar Spirit_Breaker Tidehunter Underlord Undying Wraith_King Anti-Mage Bounty_Hunter Drow_Ranger Ember_Spirit Gyrocopter Juggernaut Lone_Druid Luna Mirana Monkey_King Morphling Naga_Siren Phantom_Lancer Riki Sniper Templar_Assassin Troll_Warlord Ursa Vengeful_Spirit Chen Crystal_Maiden Disruptor Enchantress Jakiro Keeper_of_the_Light Lina Nature's_Prophet Ogre_Magi Oracle Puck Rubick Shadow_Shaman Silencer Skywrath_Mage Storm_Spirit Techies Tinker Windranger Zeus Ancient_Apparition Bane Batrider Dark_Seer Dazzle Death_Prophet Enigma Invoker Leshrac Lich Lion Necrophos Outworld_Devourer Pugna Queen_of_Pain Shadow_Demon Visage Warlock Winter_Wyvern Witch_Doctor Arc_Warden Bloodseeker Broodmother Clinkz Faceless_Void Medusa Meepo Nyx_Assassin Phantom_Assassin Razor Shadow_Fiend Slark Spectre Terrorblade Venomancer Viper Weaver
 ]
 
@@ -23,20 +28,22 @@ heroes.each do |hero|
   attack_type = attackrangehold.gsub!(/[^A-Za-z]/, '')
   attack_type.slice!("abbr")
 
-  hash_new[:t1left].tr('\'', '').slice!("talent")
-  hash_new[:t1right].tr('\'', '').slice!("talent")
-  hash_new[:t2left].tr('\'', '').slice!("talent")
-  hash_new[:t2right].tr('\'', '').slice!("talent")
-  hash_new[:t3left].tr('\'', '').slice!("talent")
-  hash_new[:t3right].tr('\'', '').slice!("talent")
-  hash_new[:t4left].tr('\'', '').slice!("talent")
-  hash_new[:t4right].tr('\'', '').slice!("talent")
-
+  trim_talents(
+  hash_new[:t1left],
+  hash_new[:t1right],
+  hash_new[:t1right],
+  hash_new[:t2left],
+  hash_new[:t2right],
+  hash_new[:t3left],
+  hash_new[:t3right],
+  hash_new[:t4left],
+  hash_new[:t4right],
+  )
   Hero.create!(name: hash_new[:heroname], base_str: hash_new[:basestr], add_str: hash_new[:strgain], base_agi: hash_new[:baseagi], add_agi: hash_new[:agigain], base_int: hash_new[:baseint], add_int: hash_new[:intgain], move_speed: hash_new[:basespeed], turn_rate: hash_new[:basespeed], attack_type: attack_type, hero_type: hash_new[:mainattribute], attack_range: attack_range, talents: {
-    level10: [hash_new[:t1left], hash_new[:t1right]],
-    level15: [hash_new[:t2left], hash_new[:t2right]],
-    level20: [hash_new[:t3left], hash_new[:t3right]],
-    level25: [hash_new[:t4left], hash_new[:t4right]]
+    level_10: [hash_new[:t1left], hash_new[:t1right]],
+    level_15: [hash_new[:t2left], hash_new[:t2right]],
+    level_20: [hash_new[:t3left], hash_new[:t3right]],
+    level_25: [hash_new[:t4left], hash_new[:t4right]]
   })
 end
 Hero.reindex
