deed57ee6ba453573b0465bbff14af258dacf435
ZainW <zainthegreatcb@gmail.com>
2017-05-02 14:17:46 -0400

    hero searches with aliases is a GO

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,8 +1,9 @@
+require 'CSV'
 class Hero < ApplicationRecord
   validates_presence_of :name, :base_str, :base_agi, :base_int, :add_str, :add_agi, :add_int, :move_speed, :turn_rate, :attack_type, :hero_type, :attack_range
 
   has_many :abilities
-  searchkick
+  searchkick synonyms: -> {CSV.read(Rails.root.join('lib', 'synonyms', 'hero_aliases.csv'))}
   def search_data
     {
       name: name
