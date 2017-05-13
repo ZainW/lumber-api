4b4e05a6256293d07dda80d39e5085cd53499fba
ZainW <zainthegreatcb@gmail.com>
2017-05-13 16:37:37 -0400

    does this still work?

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,4 +1,3 @@
-require 'CSV'
 class Hero < ApplicationRecord
   validates_presence_of :name, :base_str, :base_agi, :base_int, :add_str, :add_agi, :add_int, :move_speed, :turn_rate, :attack_type, :hero_type, :attack_range
 
